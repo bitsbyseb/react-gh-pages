7541d376b1112ed769c78cc4106257d048e4ffeb
Johan Puentes <puentespulidojohansebastian@gmail.com>
2023-08-05 19:26:21 -0500

    .

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'https://sebastianpuentespulido.github.io/',
+  base:'https://sebastianpuentespulido.github.io/react-gh-pages/',
   plugins: [react()],
 })
